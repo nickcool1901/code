807e6e7abc208e2e4d03921aec8498c47bbb98c6
Nikita <128256104+nickcool1901@users.noreply.github.com>
2025-06-08 03:03:35 +0300

    recursion

---
+++ recursion_app/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox txtDepth1;
+        private System.Windows.Forms.Button btnDrawA1;
+        private System.Windows.Forms.Button btnDrawB1;
+        private System.Windows.Forms.Panel drawPanel1;
+        private System.Windows.Forms.Label lblDepth1;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+        private void InitializeComponent()
+        {
+            this.txtDepth1 = new System.Windows.Forms.TextBox();
+            this.btnDrawA1 = new System.Windows.Forms.Button();
+            this.btnDrawB1 = new System.Windows.Forms.Button();
+            this.drawPanel1 = new System.Windows.Forms.Panel();
+            this.lblDepth1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtDepth1
+            // 
+            this.txtDepth1.Location = new System.Drawing.Point(130, 10);
+            this.txtDepth1.Name = "txtDepth1";
+            this.txtDepth1.Size = new System.Drawing.Size(50, 20);
+            this.txtDepth1.TabIndex = 1;
+            // 
+            // btnDrawA1
+            // 
+            this.btnDrawA1.Location = new System.Drawing.Point(200, 10);
+            this.btnDrawA1.Name = "btnDrawA1";
+            this.btnDrawA1.Size = new System.Drawing.Size(75, 23);
+            this.btnDrawA1.TabIndex = 2;
+            this.btnDrawA1.Text = "Zīmēt A";
+            // 
+            // btnDrawB1
+            // 
+            this.btnDrawB1.Location = new System.Drawing.Point(280, 10);
+            this.btnDrawB1.Name = "btnDrawB1";
+            this.btnDrawB1.Size = new System.Drawing.Size(75, 23);
+            this.btnDrawB1.TabIndex = 3;
+            this.btnDrawB1.Text = "Zīmēt B";
+            // 
+            // drawPanel1
+            // 
+            this.drawPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.drawPanel1.Location = new System.Drawing.Point(10, 50);
+            this.drawPanel1.Name = "drawPanel1";
+            this.drawPanel1.Size = new System.Drawing.Size(750, 500);
+            this.drawPanel1.TabIndex = 4;
+            // 
+            // lblDepth1
+            // 
+            this.lblDepth1.Location = new System.Drawing.Point(10, 10);
+            this.lblDepth1.Name = "lblDepth1";
+            this.lblDepth1.Size = new System.Drawing.Size(100, 23);
+            this.lblDepth1.TabIndex = 0;
+            this.lblDepth1.Text = "Rekursijas dziļums:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(813, 578);
+            this.Controls.Add(this.lblDepth1);
+            this.Controls.Add(this.txtDepth1);
+            this.Controls.Add(this.btnDrawA1);
+            this.Controls.Add(this.btnDrawB1);
+            this.Controls.Add(this.drawPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+        #endregion
+    }
+}
---
+++ recursion_app/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private int recursionDepth = 0;
+        private string lastButtonClicked = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            btnDrawA1.Click += BtnDrawA1_Click;
+            btnDrawB1.Click += BtnDrawB1_Click;
+            drawPanel1.Paint += DrawPanel1_Paint;
+        }
+
+        private void BtnDrawA1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtDepth1.Text, out recursionDepth) && recursionDepth > 0)
+            {
+                lastButtonClicked = "A";
+                drawPanel1.Invalidate();
+            }
+            else
+            {
+                MessageBox.Show("Lūdzu, ievadiet derīgu rekursijas dziļumu!");
+            }
+        }
+
+        private void BtnDrawB1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtDepth1.Text, out recursionDepth) && recursionDepth > 0)
+            {
+                lastButtonClicked = "B";
+                drawPanel1.Invalidate();
+            }
+            else
+            {
+                MessageBox.Show("Lūdzu, ievadiet derīgu rekursijas dziļumu!");
+            }
+        }
+
+        private void DrawPanel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.Clear(Color.White);
+
+            if (lastButtonClicked == "A")
+            {
+                DrawA(g, drawPanel1.Width / 2, drawPanel1.Height / 2, recursionDepth, drawPanel1.Width / 4);
+            }
+            else if (lastButtonClicked == "B")
+            {
+                DrawB(g, drawPanel1.Width / 2, drawPanel1.Height / 2, recursionDepth, drawPanel1.Width / 3, 0);
+            }
+        }
+
+        private void DrawA(Graphics g, int x, int y, int depth, int size)
+        {
+            if (depth <= 0) return;
+
+            g.DrawEllipse(Pens.Black, x - size / 2, y - size / 2, size, size);
+
+            int newSize = size / 2;
+            DrawA(g, x - newSize, y, depth - 1, newSize);
+            DrawA(g, x + newSize, y, depth - 1, newSize);
+            DrawA(g, x, y - newSize, depth - 1, newSize);
+            DrawA(g, x, y + newSize, depth - 1, newSize);
+        }
+
+        private void DrawB(Graphics g, int x, int y, int depth, int size, float rotation)
+        {
+            if (depth <= 0) return;
+
+            GraphicsState state = g.Save();
+
+            Matrix transform = new Matrix();
+            transform.RotateAt(rotation, new PointF(x, y));
+            g.Transform = transform;
+
+            g.DrawRectangle(Pens.Black, x - size / 2, y - size / 2, size, size);
+
+            g.Restore(state);
+
+            int newSize = size * 7 / 10;
+            DrawB(g, x, y, depth - 1, newSize, rotation + 45);
+        }
+    }
+}
